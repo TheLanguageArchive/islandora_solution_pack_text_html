ec0c7b374bf838cfb08039c0157118cce609bc2b
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2018-09-20 10:30:07 +0200

    adding text/hmtl viewer

---
+++ theme/islandora-sp-text-html-view.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * This is the template file for the object page for Text/HTML objects
+ *
+ */
+?>
+<div class="islandora-sp-text-html islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="TEXTHTMLOBJECT">
+    <?php echo $content; ?>
+</div>
