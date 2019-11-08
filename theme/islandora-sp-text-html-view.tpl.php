789360a70c28513242f31b224de92df75f63ec4b
Paul Trilsbeek <Paul.Trilsbeek@mpi.nl>
2019-11-08 12:09:35 +0100

    updated template for when content is not accessible

--- theme/islandora-sp-text-html-view.tpl.php
+++ theme/islandora-sp-text-html-view.tpl.php
@@ -5,6 +5,10 @@
  *
  */
 ?>
-<div class="islandora-sp-text-html islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="TEXTHTMLOBJECT">
-    <?php echo $islandora_content; ?>
-</div>
+<?php if (!empty($islandora_content)): ?>
+    <div class="islandora-sp-text-html islandora" vocab="http://schema.org/" prefix="dcterms: http://purl.org/dc/terms/" typeof="TEXTHTMLOBJECT">
+        <?php echo $islandora_content; ?>
+    </div>
+<?php else: ?>
+    <p>This file is not accessible to you.</p>
+<?php endif; ?>
